ea25567b28c2386323b7a62a2b1cc32673bf8f13
guyera <aguyer@chemeketa.edu>
2024-02-14 18:21:18 -0800

    Add code from 2/14 lecture

--- githubSearch/githubSearch.test.js
+++ githubSearch/githubSearch.test.js
@@ -13,14 +13,45 @@ import "@testing-library/jest-dom"
 import domTesting from "@testing-library/dom"
 import userEvent from"@testing-library/user-event"
 
+import { http, HttpResponse } from "msw"
+import { setupServer } from "msw/node"
+
+import { createRequire } from "module"
+const require = createRequire(import.meta.url)
+
+const searchRepositoriesResult = require("./searchRepositoriesResult.json")
+
+const server = setupServer(
+	http.get("https://api.github.com/search/repositories", function({params}) {
+		console.log("Fake API / intercept is called")
+		return HttpResponse.json(searchRepositoriesResult)
+	})
+)
+
+
+beforeAll(function() {
+	server.listen()
+})
+
+beforeEach(function() {
+	jest.resetModules()
+})
+
+afterEach(function() {
+	server.resetHandlers()
+})
+
+afterAll(function() {
+	server.close()
+})
+
 function initDomFromFiles(htmlPath, jsPath) {
     const html = fs.readFileSync(htmlPath, 'utf8')
     document.open()
     document.write(html)
     document.close()
-    jest.isolateModules(function () {
-        import(jsPath)
-    })
+	console.log(jsPath)
+    import(jsPath)
 }
 
 test("correctly renders GitHub search results", async function () {
@@ -40,5 +71,27 @@ test("correctly renders GitHub search results", async function () {
     await user.click(searchButton)
 
     const results = await domTesting.findAllByRole(document, "listitem")
-    expect(results).not.toHaveLength(0)
+	expect(results).toHaveLength(searchRepositoriesResult.items.length)
+	expect(results[0]).toHaveTextContent(searchRepositoriesResult.items[0].full_name)
+})
+
+test("correctly 2 renders GitHub search results", async function () {
+    initDomFromFiles(
+        __dirname + "/githubSearch.html",
+        __dirname + "/githubSearch.js"
+    )
+
+    const queryInput = domTesting.getByPlaceholderText(
+        document,
+        "Search GitHub"
+    )
+    const searchButton = domTesting.getByRole(document, "button")
+
+    const user = userEvent.setup()
+    await user.type(queryInput, "jest")
+    await user.click(searchButton)
+
+    const results = await domTesting.findAllByRole(document, "listitem")
+	expect(results).toHaveLength(searchRepositoriesResult.items.length)
+	expect(results[0]).toHaveTextContent(searchRepositoriesResult.items[0].full_name)
 })
